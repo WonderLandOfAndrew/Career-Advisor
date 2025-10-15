1a4a4543463aaa722b3e667db0d3ac97df785534
Albert <gamescellrblx@protonmail.com>
2025-10-15 15:36:34 +0300

    Third iteration
    Improved the facts and rules - taking advantage of the other operators, such as Disjunction(OR). Implication is already implemented, as this uses "if... then..." property.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,24 @@ st.write("Tick the statements that are true for you, then click **Recommend**.")
 
 # Build the facts from checkboxes (names MUST match your rules)
 facts = {
-    "likes_helping_people": st.checkbox("I like helping people.", value=True),
-    "likes_maths":          st.checkbox("I like maths."),
-    "likes_creativity":     st.checkbox("I enjoy creative work."),
-    "likes_law":            st.checkbox("I'm interested in law.", value=True),
-    "likes_science":        st.checkbox("I like science."),
-    "likes_discipline":     st.checkbox("I value discipline and structure."),
-    "dislikes_blood":       st.checkbox("I dislike seeing blood."),
-    "likes_children":       st.checkbox("I like working with children.", value=True),
+    "likes_helping_people"  : st.checkbox("I like helping people.", value=True),
+    "likes_maths"           : st.checkbox("I like maths."),
+    "likes_creativity"      : st.checkbox("I enjoy creative work."),
+    "likes_law"             : st.checkbox("I'm interested in law.", value=True),   
+    "likes_science"         : st.checkbox("I like science."),
+    "likes_discipline"      : st.checkbox("I value discipline and structure."),
+    "dislikes_blood"        : st.checkbox("I dislike seeing blood."),
+    "likes_children"        : st.checkbox("I like working with children.", value=True),
+    
+    "good_communication"    : st.checkbox("I am a good communicator."),
+    "likes_technology"      : st.checkbox("I like technology."),
+    "likes_problem_solving" : st.checkbox("I enjoy solving problems."),
+    "prefers_teamwork"      : st.checkbox("I prefer working in a team."),
+    "likes_outdoors"        : st.checkbox("I enjoy outdoor activities."),
+    "likes_research"        : st.checkbox("I enjoy researching."),
+    "high_attention_detail" : st.checkbox("I pay attention to details."),
+    "likes_leadership"      : st.checkbox("I am a confident team leader.")
+
 }
 
 if st.button("Recommend"):
